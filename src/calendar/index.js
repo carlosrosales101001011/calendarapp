8a3051106eec6761253761b626825c5c330b6a96
Carlos Rosales <Carlosrosales21092002@hotmail.com>
2023-01-15 09:34:50 -0500

    Actualizando la vista, mes, dia y agenda: Poniedo la vista en localStorage

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -2,3 +2,4 @@
 
 export * from './components/NavBar'
 export * from './pages/CalendarPage'
+export * from './components/CalendarEvent'
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,10 @@
 import { Calendar } from 'react-big-calendar'
-import { NavBar } from '../'
+import { CalendarEvent, NavBar } from '../'
 
 import {addHours} from 'date-fns'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { getMessageEs, localizer } from '../../helpers';
+import { useState } from 'react';
 const events = [
     {
         title: "Cumple del jefe",
@@ -11,7 +12,10 @@ const events = [
         start: new Date(),
         end: addHours(new Date(), 2),
         bgColor: '#FFFFFF',
-
+      user:{
+        _id: '123',
+        name: 'John',
+      }
     }
 ]
 
@@ -19,8 +23,9 @@ const events = [
 
 export const CalendarPage = () => {
 
+  const [lastView, setlastView] = useState(localStorage.getItem('lastView') || 'agenda')
   const eventPropGetter =( event, start, end, isSelected )=>{
-    console.log({event, start, end, isSelected});
+    // console.log({event, start, end, isSelected});
     const style ={
       backgroundColor:"#347CF7",
       borderRadius: '0px',
@@ -31,12 +36,27 @@ export const CalendarPage = () => {
       style
     }
   }
+
+  const onDoubleClick = (event)=>{
+    console.log({doubleClick:event});
+  }
+
+  const onSelect = (event)=>{
+    console.log({click:event});
+  }
+  
+  const onViewChange = (event)=>{
+    localStorage.setItem('lastView', event)
+    console.log({viewChange:event});
+  }
+
   return (
     <>
     <NavBar/>
 
     <Calendar
       culture='es'
+      defaultView= { lastView }
       localizer={localizer}
       events={events}
       startAccessor="start"
@@ -44,6 +64,14 @@ export const CalendarPage = () => {
       style={{ height: 500 }}
       messages= {getMessageEs()}
       eventPropGetter={eventPropGetter}
+      components={
+        {
+          event: CalendarEvent
+        }
+      }
+      onDoubleClickEvent={ onDoubleClick }
+      onSelectEvent={ onSelect }
+      onView={ onViewChange }
     />
     </>
   )
