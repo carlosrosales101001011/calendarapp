6f6ff92c16c31cd29c196877f281967546089cb3
Carlos Rosales <Carlosrosales21092002@hotmail.com>
2023-01-15 09:00:22 -0500

    Actualizando el idioma de ingles a español y agregando estilos al evento festivo

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,9 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import enUS from 'date-fns/locale/en-US'
-import {format, parse, startOfWeek, getDay, addHours} from 'date-fns'
+import { Calendar } from 'react-big-calendar'
 import { NavBar } from '../'
 
+import {addHours} from 'date-fns'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+import { getMessageEs, localizer } from '../../helpers';
 const events = [
     {
         title: "Cumple del jefe",
@@ -27,17 +15,35 @@ const events = [
     }
 ]
 
+
+
 export const CalendarPage = () => {
+
+  const eventPropGetter =( event, start, end, isSelected )=>{
+    console.log({event, start, end, isSelected});
+    const style ={
+      backgroundColor:"#347CF7",
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: '#FFFFFF'
+    }
+    return {
+      style
+    }
+  }
   return (
     <>
     <NavBar/>
 
     <Calendar
+      culture='es'
       localizer={localizer}
       events={events}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
+      messages= {getMessageEs()}
+      eventPropGetter={eventPropGetter}
     />
     </>
   )
