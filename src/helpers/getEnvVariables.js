6a2eaf8cb09528fa0701f02ef78485d8a991384f
Carlos Rosales <Carlosrosales21092002@hotmail.com>
2023-01-20 11:37:21 -0500

    Solucionando errores 2

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,7 +1,8 @@
 export const getEnvVariables = ()=>{
-    import.meta?.env;
-
-    return {
-        ...import.meta.env
-    }
+    // console.log("process", import.meta.env);
+  if (typeof process !== "undefined") {
+    return { ...process.env}
+  } else {
+    return { ...import.meta.env}
+  }
 }
